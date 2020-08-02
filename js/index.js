05b1ac46c6f65cd9f97237a7f8472fb19d35926e
unknown <sthitpanth01@gmail.com>
2020-08-02 20:50:40 +0600

    form validation added

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ const email = document.querySelector('.btn2');
 const code = document.querySelector('.btn');
 const emailIcon = document.querySelector('.icon-email');
 const githubIcon = document.querySelector('.icon-github');
-
+const submit = document.querySelector('.submit');
 
 function toggleHeight() {
     nav.classList.toggle('collapse');
@@ -18,7 +18,31 @@ function checked() {
     document.body.classList.toggle('dark0');
     nav.classList.toggle('dark');
     footer.classList.toggle('dark1');
+}
 
+function logErr() {
+    let name = document.querySelector('.input1').value;
+    let emailMe = document.querySelector('.input2').value;
+    let textarea = document.querySelector('.textarea').value;
+    let errLog = document.querySelector('#log-err');
+
+    errLog.style.color = 'maroon';
+    if (name.length <= 0) {
+        errLog.textContent = "Name is too short.";
+        return false;
+    } else if (emailMe <= 0 || emailMe.indexOf('@') == -1) {
+        errLog.textContent = "Email is not correct.";
+        return false;
+    }
+    else if (textarea.length <= 0) {
+        errLog.textContent = "Send me a hello at least! :)";
+        return false;
+    } else {
+        setTimeout(function(){
+            errLog.textContent = " ";
+        }, 3000)
+        errLog.textContent = `Thanks for reaching out me. Good day, ${name}!`;
+    }
 }
 
 function emailfunc() {
@@ -62,3 +86,4 @@ email.addEventListener('click', emailfunc);
 code.addEventListener('click', githubCode);
 emailIcon.addEventListener('click', emailfunc);
 githubIcon.addEventListener('click', githubfunc);
+submit.addEventListener('click', logErr);
